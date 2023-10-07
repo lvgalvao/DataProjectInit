1260b00d4f8b98f8bf7a0f8ed41c49d5c5dab82c
Luciano Filho <lvgalvaofilho@gmail.com>
2023-10-06 23:48:45 -0300

    add comentarios

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-import glob
-import pandas as pd
+import os # usada para manipular diretórios e arquivos
+import glob # usada para listar arquivos em um diretório
+import pandas as pd # usada para manipular dados
 
 # Configurações de diretórios e nomes de arquivos
 input_folder = "data/input"  # Altere para o caminho da sua pasta de entrada
@@ -8,10 +8,13 @@ output_folder = "data/output"    # Altere para o caminho da sua pasta de saída
 output_file_name = "consolidado.xlsx"
 
 # Extract
-files = glob.glob(os.path.join(input_folder, "*.xlsx"))
+files = glob.glob(os.path.join(input_folder, "*.xlsx")) # lista todos os arquivos .xlsx do diretório
 if not files:
     raise ValueError("No Excel files found in the specified folder")
-all_data = [pd.read_excel(file) for file in files]
+
+all_data = [pd.read_excel(file) for file in files] # lê cada arquivo e armazena em uma lista
+
+print(all_data)
 
 if not all_data:
     raise ValueError("No data to transform")
